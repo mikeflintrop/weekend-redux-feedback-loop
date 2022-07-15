5cbcba98cd411a148c02ebfb3b6404c64c7a264d
Mike Flintrop <mike.flintrop@gmail.com>
2022-07-15 16:21:12 -0500

    started index template

--- src/index.js
+++ src/index.js
@@ -4,5 +4,84 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+// Reducers
+const feelingReducer = (state = 0, action) => {
+    console.log('Feeling reducer: ', action)
+    switch (action.type) {
+        case 'ADD_FEELING':
+        console.log('You clicked ADD_FEELING', action.payload);
+        return action.payload;
+        default: 
+        // if action.type is anything else, return last value of state
+        return state;
+    }
+}
+
+const understandingReducer = (state = 0, action) => {
+    console.log('Understanding reducer: ', action)
+    switch (action.type) {
+        case 'ADD_UNDERSTANDING':
+        console.log('You clicked ADD_UNDERSTANDING', action.payload);
+        return action.payload;
+        default: 
+        // if action.type is anything else, return last value of state
+        return state;
+    }
+}
+
+const supportedReducer = (state = 0, action) => {
+    console.log('Supported reducer: ', action)
+    switch (action.type) {
+        case 'ADD_SUPPORTED':
+        console.log('You clicked ADD_SUPPORTED', action.payload);
+        return action.payload;
+        default: 
+        // if action.type is anything else, return last value of state
+        return state;
+    }
+}
+
+const commentsReducer = (state = 0, action) => {
+    console.log('Comments reducer: ', action)
+    switch (action.type) {
+        case 'ADD_COMMENTS':
+        console.log('You clicked ADD_COMMENTS', action.payload);
+        return action.payload;
+        default: 
+        // if action.type is anything else, return last value of state
+        return state;
+    }
+}
+
+const feedbackReducer = (state = 0, action) => {
+    console.log('Feedback reducer: ', action)
+    switch (action.type) {
+        case 'ADD_FEEDBACK':
+        console.log('You clicked ADD_FEEDBACK', action.payload);
+        return action.payload;
+        default: 
+        // if action.type is anything else, return last value of state
+        return state;
+    }
+}
+
+// Sore
+const storeInstance = createStore(
+    combineReducers({
+        feelingReducer,
+        understandingReducer,
+        supportedReducer,
+        commentsReducer,
+        feedbackReducer
+    }),
+    applyMiddleware(logger)
+);
+
+// Provider
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
